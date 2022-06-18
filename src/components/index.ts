c47bda219cd552429545273aefb31d76729749be
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-06-19 00:07:43 +0800

    feat: add support for modals

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './button.js'
 export * from './embed.js'
+export * from './modals.js'
 export * from './rows.js'
---
+++ src/components/modals.tsx
@@ -0,0 +1,58 @@
+import type { MessageActionRow, TextInputStyle } from 'discord.js'
+import { Modal as _Modal, TextInputComponent } from 'discord.js'
+import type { JSXString } from '../utils.js'
+import { arrayify, normalize } from '../utils.js'
+import { ActionRow } from './rows.js'
+
+export interface ModalProps {
+  customId: string
+  title: string
+  children: JSX.Element | JSX.Element[]
+}
+
+export function Modal(props: ModalProps): _Modal {
+  return new _Modal()
+    .setCustomId(props.customId)
+    .setTitle(props.title)
+    .setComponents(...arrayify(props.children as MessageActionRow<TextInputComponent>))
+}
+
+export interface InputProps {
+  customId: string
+  children: JSXString
+  minLength?: number
+  maxLength?: number
+  placeholder?: string
+  required?: boolean
+  type?: Lowercase<TextInputStyle>
+  value?: string
+}
+
+export function Input(props: InputProps): JSX.Element {
+  const input = new TextInputComponent()
+    .setCustomId(props.customId)
+    .setLabel(normalize(props.children))
+    .setStyle((props.type ?? 'short').toUpperCase() as TextInputStyle)
+
+  if (props.minLength !== undefined) {
+    input.setMinLength(props.minLength)
+  }
+
+  if (props.maxLength !== undefined) {
+    input.setMaxLength(props.maxLength)
+  }
+
+  if (props.placeholder !== undefined) {
+    input.setPlaceholder(props.placeholder)
+  }
+
+  if (props.required !== undefined) {
+    input.setRequired(props.required)
+  }
+
+  if (props.value !== undefined) {
+    input.setValue(props.value)
+  }
+
+  return <ActionRow>{input}</ActionRow>
+}
