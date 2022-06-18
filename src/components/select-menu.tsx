4ed3ecd75d849ac5fe48182e9e2a6be476433f12
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-06-19 00:28:48 +0800

    feat: add support for select menus

---
+++ src/components/select-menu.tsx
@@ -0,0 +1,40 @@
+import type { EmojiIdentifierResolvable, MessageSelectOptionData } from 'discord.js'
+import { MessageSelectMenu } from 'discord.js'
+import type { JSXString } from '../utils'
+import { arrayify, normalize } from '../utils'
+import { ActionRow } from './rows'
+
+export interface SelectMenuProps {
+  customId: string
+  children: JSX.Element | JSX.Element[]
+  disabled?: boolean
+  maxValues?: number
+  minValues?: number
+  placeholder?: string
+}
+
+export function SelectMenu(props: SelectMenuProps): JSX.Element {
+  const menu = new MessageSelectMenu()
+    .setCustomId(props.customId)
+    .setOptions(arrayify(props.children as MessageSelectOptionData))
+
+  return <ActionRow>{menu}</ActionRow>
+}
+
+export interface OptionProps {
+  value: string
+  children: JSXString
+  selected?: boolean
+  description?: string
+  emoji?: EmojiIdentifierResolvable
+}
+
+export function Option(props: OptionProps): MessageSelectOptionData {
+  return {
+    value: props.value,
+    label: normalize(props.children),
+    default: props.selected,
+    description: props.description,
+    emoji: props.emoji,
+  }
+}
